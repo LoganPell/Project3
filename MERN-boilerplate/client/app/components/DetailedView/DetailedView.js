0a24c498afb042caebc12cbc560532b4e52f0d3a
Mike Vollbracht <mike.vollbracht@gmail.com>
2018-04-10 22:05:06 -0500

    updated css for detailed view - sidebar and content areas

--- MERN-boilerplate/client/app/components/DetailedView/DetailedView.js
+++ MERN-boilerplate/client/app/components/DetailedView/DetailedView.js
@@ -6,11 +6,11 @@ const DetailedView = () => (
 
 	<div className="row">
 		<div id="sidebar" className="col s3">
-			<p>Sidebar</p>
+			<p>Sidebar Area</p>
 		</div>
 
 		<div id="main" className="col s9">
-			<p>Main Content</p>
+			<p>Main Content Area</p>
 		</div>
 	</div>
 </div>
