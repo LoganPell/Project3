06dbe17cbfe3f410bc80edfc8ea33a69234e552b
Mike Vollbracht <mike.vollbracht@gmail.com>
2018-04-13 22:21:02 -0500

    working on details

--- MERN-boilerplate/client/app/components/DetailedView/DetailedView.js
+++ MERN-boilerplate/client/app/components/DetailedView/DetailedView.js
@@ -1,23 +1,60 @@
 import React, { Component } from 'react';
 
-const DetailedView = (props) => (
 
-	<div>
+class DetailedView extends React.Component {
+	constructor(props) {
+    super(props);
 
-	<div className="row">
-		<div id="sidebar">
-			<div id="actionForm" className="z-depth-2">
-				
+    this.state = {
+      active: null
+    }
+
+    this.toggle = this.toggle.bind(this);
+    this.activeItem = this.activeItem.bind(this);
+  }
+
+	toggle(position) {
+     this.setState({active : position})
+  }
+  
+  activeItem(position) {
+    if (this.state.active === position) {
+      return "formSelectActive";
+    }
+    return "formSelect";
+  }
+
+
+
+	render(){
+		return (
+			<div>
+				<div className="row">
+					<div id="sidebar">
+						<div>
+							<ul id="sidebarSelect" className="center-align">
+								<li className={this.activeItem(0)} onClick={() => {this.toggle(0)}}>Income</li>
+       					<li className={this.activeItem(1)} onClick={() => {this.toggle(1)}}>Bills</li>
+       					<li className={this.activeItem(2)} onClick={() => {this.toggle(2)}}>Goals</li>
+							</ul>
+						</div>
+						<div id="sideForm">
+							<form>
+							  <label>
+							    Name:
+							    <input className="myInput" type="text" name="name"/>
+							  </label>
+							</form>
+						</div>
+					</div>
+
+					<div id="main" className="">
+						<h4>$500.00</h4>
+						<p>Main Content Area</p>
+					</div>
+				</div>
 			</div>
-		</div>
+	)};
 
-		<div id="main" className="">
-			<h4>$500.00</h4>
-			<p>Main Content Area</p>
-		</div>
-	</div>
-</div>
-
-);
-
+}
 export default DetailedView;
