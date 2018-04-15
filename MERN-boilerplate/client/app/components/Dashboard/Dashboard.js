b6ec00cbc74d103ce74f3da786e7c514fe9bc1e5
Caleb Rose <clrose86@gmail.com>
2018-04-15 12:58:01 -0500

    dashboard revamped to actually utilize React

--- MERN-boilerplate/client/app/components/Dashboard/Dashboard.js
+++ MERN-boilerplate/client/app/components/Dashboard/Dashboard.js
@@ -1,67 +1,32 @@
 import React, { Component } from 'react';
+import DashCard from "./DashCard.js";
+import cards from "./cards.json";
 
+class Dashboard extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			cards
+		}
+	}
+	render(){
+		return(
+			<div>
+				<div className="row">
+					{this.state.cards.map(card =>(
+						<DashCard
+							id={card.id}
+							key={card.id}
+							name={card.name}
+							image={card.image}
+							icon={card.icon}
+						/>
+					))}
+				</div>
+			</div>
+		);
+	}
+} 
 
-const Dashboard = (props) => (
-	<div>
-		<div className="row">
-			<div className="col s10 m4 l4 offset-s1">
-				<div className="card">
-					<div className="card-image">
-						<img className="imgs" src="/assets/img/wallet.jpg"/>
-						<object className="icons" data="/assets/img/income.svg" type="image/svg+xml"></object>
-					</div>
-					<div className="card-content">
-						<span className="card-title">Income</span>
-						<p>This will be the props for income</p>
-					</div>
-					<div className="card-action">
-						<a className="waves-effect waves-teal btn-flat green accent-2 white-text" id="incomeBtn" href="#">
-							<i className="material-icons left">more_horiz</i>
-							More
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="col s10 m4 l4 offset-s1">
-				<div className="card">
-					<div className="card-image">
-						<img className="imgs" src="/assets/img/bills.jpg"/>
-						<object className="icons" data="/assets/img/bills.svg" type="image/svg+xml"></object>
-					</div>
-					<div className="card-content">
-						<span className="card-title">Bills</span>
-						<p>This will be the props for income</p>
-					</div>
-					<div className="card-action">
-						<a className="waves-effect waves-light btn" href="#">
-							<i className="material-icons left">more_horiz</i>
-							More
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="col s10 m4 l4 offset-s1">
-				<div className="card">
-					<div className="card-image">
-						<span className="base">
-							<img className="imgs" src="/assets/img/wallet.jpg"/>
-						</span>
-						<span className="overlay"></span>
-					</div>
-					<div className="card-content">
-						<span className="card-title">Goals</span>
-						<p>This will be the props for income</p>
-					</div>
-					<div className="card-action">
-						<a className="waves-effect waves-light btn" href="#">
-							<i className="material-icons left">more_horiz</i>
-							More
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-);
 
 export default Dashboard;
