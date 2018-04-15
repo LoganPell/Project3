abfe9072b81c98c49fade803f6a78684408f4ce3
Caleb Rose <clrose86@gmail.com>
2018-04-15 11:00:36 -0500

    styling added, dashboard cards made

--- MERN-boilerplate/client/app/components/Dashboard/Dashboard.js
+++ MERN-boilerplate/client/app/components/Dashboard/Dashboard.js
@@ -1,8 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
-const Dashboard = () => (
-	<p>Yay Dashboard</p>
+
+const Dashboard = (props) => (
+	<div>
+		<div className="row">
+			<div className="col s10 m4 l4 offset-s1">
+				<div className="card">
+					<div className="card-image">
+						<img className="imgs" src="/assets/img/wallet.jpg"/>
+						<object className="icons" data="/assets/img/income.svg" type="image/svg+xml"></object>
+					</div>
+					<div className="card-content">
+						<span className="card-title">Income</span>
+						<p>This will be the props for income</p>
+					</div>
+					<div className="card-action">
+						<a className="waves-effect waves-teal btn-flat green accent-2 white-text" id="incomeBtn" href="#">
+							<i className="material-icons left">more_horiz</i>
+							More
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col s10 m4 l4 offset-s1">
+				<div className="card">
+					<div className="card-image">
+						<img className="imgs" src="/assets/img/bills.jpg"/>
+						<object className="icons" data="/assets/img/bills.svg" type="image/svg+xml"></object>
+					</div>
+					<div className="card-content">
+						<span className="card-title">Bills</span>
+						<p>This will be the props for income</p>
+					</div>
+					<div className="card-action">
+						<a className="waves-effect waves-light btn" href="#">
+							<i className="material-icons left">more_horiz</i>
+							More
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col s10 m4 l4 offset-s1">
+				<div className="card">
+					<div className="card-image">
+						<span className="base">
+							<img className="imgs" src="/assets/img/wallet.jpg"/>
+						</span>
+						<span className="overlay"></span>
+					</div>
+					<div className="card-content">
+						<span className="card-title">Goals</span>
+						<p>This will be the props for income</p>
+					</div>
+					<div className="card-action">
+						<a className="waves-effect waves-light btn" href="#">
+							<i className="material-icons left">more_horiz</i>
+							More
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 );
 
 export default Dashboard;
--- MERN-boilerplate/client/app/components/Header/Header.js
+++ MERN-boilerplate/client/app/components/Header/Header.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <nav>
+   
       <Link to="/">Home</Link>
       <Link to="/helloworld">Hello World</Link>
       <Link to="/detailed">Detailed View</Link>
-    </nav>
+      <Link to="/dashboard">Dashboard</Link>
+   
 
     <hr />
   </header>
