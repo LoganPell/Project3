6d68e9bc16f6c3f5b1c7bf536b902233512804a2
Mike Vollbracht <mike.vollbracht@gmail.com>
2018-04-15 20:46:06 -0500

    added database functionality

---
+++ MERN-boilerplate/server/routes/api/data.js
@@ -0,0 +1,98 @@
+const doughData = require('../../models/doughData');
+
+module.exports = (app) => {
+  
+  //post original form data to database
+  app.post('/api/data/add', (req, res, next) => {
+    const {body} = req;
+    const {userID} = body;
+    const {dataType} = body;
+    const {dataDesc} = body;
+    const {dataAmount} = body;
+    const {dataDate} = body;
+    const {dataReccurance} = body;
+
+    //making sure all fields are filled out
+    if (!userID) {
+      return res.send({
+        success: false,
+        message: 'Error: need username.'
+      });
+    }
+    if (!dataDesc) {
+      return res.send({
+        success: false,
+        message: 'Error: need description.'
+      });
+    }
+    if (!dataAmount) {
+      return res.send({
+        success: false,
+        message: 'Error: need description.'
+      });
+    }
+    if (!dataDate) {
+      return res.send({
+        success: false,
+        message: 'Error: need description.'
+      });
+    }
+
+    //creating new document to add to data base
+    const newDoughData = new doughData();
+    newDoughData.userID = userID;
+    newDoughData.dataType = dataType;
+    newDoughData.dataDesc = dataDesc;
+    newDoughData.dataAmount = dataAmount;
+    newDoughData.dataDate = dataDate;
+    newDoughData.dataReccurance = dataReccurance;
+
+    //adding new doucment to database
+    newDoughData.save((err, doughData) => {
+        if (err) {
+          res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        }
+
+        res.send({
+          success: true,
+          message: 'data added!'
+      });
+    });
+  });
+
+  app.get('/api/data/all', (req, res, next) => {
+    //get userID
+    // const {body} = req;
+    // const {userID} = body;
+    // console.log(userID);
+    const { query } = req;
+    const { token } = query
+
+    //get user data from dough data
+    doughData.find({
+      userID: token
+    }, (err, returnedData) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      }
+      if (returnedData.length === 0) {
+        return res.send({
+          success: false,
+          message: 'No Data Found'
+        });
+      } else {
+        return res.send({
+          success: true,
+          message: 'Good to go',
+          data: returnedData
+        });
+      }
+    });
+  });
+};  
